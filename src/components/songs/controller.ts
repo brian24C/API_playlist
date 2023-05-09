2b178fda3242b7dd8705de6668a3bf9c933d5529
brian24C <brianjosuecastro@hotmail.com>
2023-05-09 15:04:07 -0500

    add songs controller

---
+++ src/components/songs/controller.ts
@@ -0,0 +1,7 @@
+import type { Request, Response } from "express";
+import { supabase } from "../../services/supabase";
+
+export const findAll_songs = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {};
