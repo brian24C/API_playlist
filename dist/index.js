d4c82926193607fb2b1aa08aa16e1e4d3c5fbbaf
brian24C <brianjosuecastro@hotmail.com>
2023-05-09 12:00:27 -0500

    install cors y add to app.ts

--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = require("dotenv");
 const app_1 = __importDefault(require("./app"));
 (0, dotenv_1.config)();
+app_1.default.get("/", (req, res) => {
+    res.send("hi");
+});
 const port = process.env.PORT || 6005;
 app_1.default.listen(port, () => {
     console.log(`Server is init at http://localhost:${port}`);
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,15 @@
 import express, { type Application } from "express";
+import cors from "cors";
 
 const app: Application = express();
+
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,PUT,POST,DELETE",
+  })
+);
+
+app.use(express.json());
 
 export default app;
