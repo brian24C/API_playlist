79b912a229eb684fee76e0bd3cb6b33f54734665
brian24C <brianjosuecastro@hotmail.com>
2023-05-09 15:07:02 -0500

    add function findAllSongs to controller Songs

--- src/components/songs/controller.ts
+++ src/components/songs/controller.ts
@@ -1,7 +1,15 @@
 import type { Request, Response } from "express";
 import { supabase } from "../../services/supabase";
 
-export const findAll_songs = async (
+export const findAllSongs = async (
   req: Request,
   res: Response
-): Promise<Response> => {};
+): Promise<Response> => {
+  try {
+    const Songs = await supabase.from("Songs").select("*");
+
+    return success({ res, data: Songs });
+  } catch (error) {
+    return failure({ res, message: error });
+  }
+};
