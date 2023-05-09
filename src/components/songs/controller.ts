1eb8621ad57c158e0894561bceef1011f9f69dc4
brian24C <brianjosuecastro@hotmail.com>
2023-05-09 15:22:46 -0500

    add findOneSong to controller songs

--- src/components/songs/controller.ts
+++ src/components/songs/controller.ts
@@ -14,3 +14,21 @@ export const findAllSongs = async (
     return failure({ res, message: error });
   }
 };
+
+export const findOneSong = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  try {
+    const id: number = parseInt(req.params.id);
+    const Song = await supabase.from("Songs").select("*").eq("id", id);
+
+    if (Song && Song.data && Song.data.length === 0) {
+      return failure({ res, message: "Song not found" });
+    }
+
+    return success({ res, message: "Song found", data: Song });
+  } catch (error) {
+    return failure({ res, message: error });
+  }
+};
