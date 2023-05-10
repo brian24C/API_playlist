1cd0a56dc6ee0ea604d11e099463bc00db62bc7a
brian24C <brianjosuecastro@hotmail.com>
2023-05-10 17:34:40 -0500

    update tsconfig

--- dist/services/supabase.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.supabase = void 0;
-const supabase_js_1 = require("@supabase/supabase-js");
-const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)();
-exports.supabase = (0, supabase_js_1.createClient)(process.env.SUPABASE_PROJECT_URL, process.env.SUPABASE_API_KEY);
