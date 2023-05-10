c076787d3a6489d162e4bdee1599736befec719b
brian24C <brianjosuecastro@hotmail.com>
2023-05-10 11:43:05 -0500

    add function findPlaylist to playlist controller

--- src/components/playlists/controller.ts
+++ src/components/playlists/controller.ts
@@ -1,9 +1,33 @@
 import type { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
+import { success, failure } from "../../responses";
 
 const prisma = new PrismaClient();
 
-export const findplaylist = async (
+export const findPlaylist = async (
   _req: Request,
   res: Response
-): Promise<void> => {};
+): Promise<Response> => {
+  try {
+    const playlists = await prisma.playlist.findMany({
+      include: {
+        songs: {
+          select: {
+            song: {
+              select: {
+                id: true,
+                name: true,
+                link: true,
+                createdBy: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    return success({ res, data: playlists });
+  } catch (error) {
+    return failure({ res, message: error });
+  }
+};
