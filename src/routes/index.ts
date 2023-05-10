5649b43c95966297be71a63ef1c526716910b124
brian24C <brianjosuecastro@hotmail.com>
2023-05-10 11:51:51 -0500

    iterate routes song and playlist

---
+++ src/routes/index.ts
@@ -0,0 +1,15 @@
+import type { Application, Router } from "express";
+import * as ROUTES from "../components";
+
+const _routes: [string, Router][] = [
+  ["song", ROUTES.SongRouter],
+  ["playlist", ROUTES.playlistRouter],
+];
+
+const routes = (app: Application): void => {
+  _routes.forEach(([path, controller]) => {
+    app.use(`/api/v1/${path}`, controller);
+  });
+};
+
+export default routes;
