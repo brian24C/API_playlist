9d4331a5790909b24b1071ae2b52efa38268a729
brian24C <brianjosuecastro@hotmail.com>
2023-06-05 11:23:46 -0500

    set up cloudinary

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express, { type Application } from "express";
+import { config } from "dotenv";
 import cors from "cors";
 import routes from "./routes";
-
+import { v2 as cloudinary } from "cloudinary";
+config();
 const app: Application = express();
 
 app.use(
@@ -12,6 +14,12 @@ app.use(
 );
 
 app.use(express.json());
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET_KEY,
+});
 
 routes(app);
 
